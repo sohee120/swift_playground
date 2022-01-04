e9a0db902e31bf5b23975971c43439aeb8ec0249
sohee120 <yuhsohee120@gmail.com>
2022-01-04 15:40:55 +0900

    feat: GoogleMap API

---
+++ GoogleMap/GoogleMap/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  GoogleMap
+//
+//  Created by 윤소희 on 2022/01/04.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //앱 구동시 맨처음 실행되는 함수
+        GMSServices.provideAPIKey("AIzaSyA_KT0ugR-U0G4iTeo2byuXr2eYC4zs3fw")
+        GMSPlacesClient.provideAPIKey("AIzaSyA_KT0ugR-U0G4iTeo2byuXr2eYC4zs3fw")
+        
+        
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ GoogleMap/GoogleMap/GoogleMapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GoogleMapViewController.swift
+//  GoogleMap
+//
+//  Created by 윤소희 on 2022/01/04.
+//
+
+import UIKit
+
+
+//개발자 콘솔
+//https://console.cloud.google.com
+
+//API키 : AIzaSyA_KT0ugR-U0G4iTeo2byuXr2eYC4zs3fw
+
+class GoogleMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+ 
+
+}
